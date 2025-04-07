01fd477293c1f25062d4881c03fbaea0ed8c9ff2
zhangsdong <zhangsdong0402@gmail.com>
2025-04-07 14:34:25 +0800

    fix(cli): fix parameter error

--- clipx/commands/parser.py
+++ clipx/commands/parser.py
@@ -11,18 +11,29 @@ def create_cli_parser():
     Create CLI parser with all options
     """
 
+    def version_callback(ctx, param, value):
+        if value:
+            from clipx import __version__
+            click.echo(f"clipx version {__version__}")
+            ctx.exit()
+        return value
+
     @click.command()
+    @click.option('-v', '--version', is_flag=True,
+                  help='Show version information.',
+                  callback=version_callback,
+                  is_eager=True,
+                  expose_value=False)
     @add_input_options
     @add_output_options
     @add_model_options
     @add_mask_options
-    @click.option('-v', '--version', is_flag=True, help='Show version information.')
     @click.help_option('-h', '--help')
     def clipx_command(**kwargs):
         """
         clipx - Image background removal tool.
         """
-        # Just collect options, actual processing happens in core.py
+        # 收集选项，实际处理在 core.py 中完成
         return kwargs
 
     return clipx_command
